39f183b0040f8926cae77b9520adeabe67114b65
bharathksunil <bharathk.sunil.k@gmail.com>
2022-04-12 02:37:39 +0530

    Cleanup

--- types/redis/index.d.ts
+++
@@ -1,8 +0,0 @@
-/// <reference types="ioredis" />
-declare const _default: {
-    cache: import("ioredis").Redis;
-    pub: import("ioredis").Redis;
-    sub: import("ioredis").Redis;
-};
-export default _default;
-//# sourceMappingURL=index.d.ts.map
--- types/redis/redis.pub.client.d.ts
+++
@@ -1,4 +0,0 @@
-import redis from 'ioredis';
-declare const _default: redis.Redis;
-export default _default;
-//# sourceMappingURL=redis.pub.client.d.ts.map
