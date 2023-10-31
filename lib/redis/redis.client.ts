3674ffd7f3952661b3b5b6e386e90bf169b98333
Bharath Kumar S <bharathk.sunil.k@gmail.com>
2023-10-31 17:37:46 +0530

    Upgrade dependencies

--- lib/redis/redis.client.ts
+++ lib/redis/redis.client.ts
@@ -1,16 +1,16 @@
-import redis from 'ioredis';
+import { Redis, RedisOptions } from 'ioredis';
 
-const options = <redis.RedisOptions>{
+const options = <RedisOptions>{
   enableOfflineQueue: false,
   retryStrategy(times: number) {
     return Math.min(times * 100, 3000);
   },
 };
 
-let redisConnection: redis.Redis | null = null;
+let redisConnection: Redis | null = null;
 
-export default ((): redis.Redis => {
+export default ((): Redis => {
   if (!redisConnection)
-    redisConnection = new redis(process.env.REDIS_CACHE_URL, options);
+    redisConnection = new Redis(process.env.REDIS_CACHE_URL!!, options);
   return redisConnection;
 })();
